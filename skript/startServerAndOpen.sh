a6f2b637542699af1c14de89fbc8a58d61f18926
Lukas Kappeler <lks3737@gmail.com>
2023-12-25 10:46:03 +0100

    skript Server

---
+++ skript/startServerAndOpen.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Pfad zum Logistiksystem-Verzeichnis
+LOGISTIKSYSTEM_DIR="/home/lukas/Logistiksystem/Logistiksystem"
+
+# Starte den Yarn-Server
+cd "$LOGISTIKSYSTEM_DIR"
+yarn server &
+
+# Warte kurz, um sicherzustellen, dass der Server gestartet wurde (kann angepasst werden)
+sleep 5
+
+# Überprüfe, ob der Webbrowser installiert ist
+if ! command -v chromium-browser &> /dev/null
+then
+    echo "Chromium-Browser ist nicht installiert. Bitte installiere ihn zuerst."
+    exit
+fi
+
+# Starte den Webbrowser im Vollbildmodus und öffne die Webseite
+chromium-browser --kiosk http://localhost:3000/
